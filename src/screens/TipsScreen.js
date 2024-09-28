fb48b0fba3a15a1dc35da6f832c529b4fde12dea
harkamal <honey.rawal89@gmail.com>
2024-09-28 23:53:27 +0200

    display message in popup

--- src/screens/TipsScreen.js
+++ src/screens/TipsScreen.js
@@ -1,12 +1,16 @@
 import React, { useState } from 'react';
-import { View, Text, StyleSheet, TextInput, TouchableOpacity, ScrollView } from 'react-native';
-import MarkdownDisplay from '../components/MarkdownDisplay'; // Make sure this component handles the markdown correctly
+import { View, Text, StyleSheet, TextInput, TouchableOpacity, Modal, ScrollView, Dimensions } from 'react-native';
+import MarkdownDisplay from '../components/MarkdownDisplay';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
+const { height: screenHeight } = Dimensions.get('window'); // Get screen height
 
 function TipsScreen() {
   const [childAge, setChildAge] = useState('');
   const [query, setQuery] = useState('');
   const [responseMessage, setResponseMessage] = useState('');
   const [isInvalidAge, setIsInvalidAge] = useState(false);
+  const [isModalVisible, setModalVisible] = useState(false);
 
   const handleSubmit = async () => {
     const currentDate = new Date();
@@ -37,7 +41,6 @@ function TipsScreen() {
       const data = await response.json();
 
       if (response.ok) {
-        // Successful response (code 200)
         setResponseMessage(data.message || ""); // Default to empty string if undefined
       } else {
         setResponseMessage("Sorry! Unable to help you now. Please submit your query again.");
@@ -48,6 +51,10 @@ function TipsScreen() {
     }
   };
 
+  const toggleModal = () => {
+    setModalVisible(!isModalVisible);
+  };
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Parenting Assistant!</Text>
@@ -56,7 +63,6 @@ function TipsScreen() {
       <Text style={styles.label}>Child's Age (YYYYMM):</Text>
       <TextInput
         style={styles.input}
-        placeholder="Enter child's age (YYYYMM)"
         value={childAge}
         onChangeText={setChildAge}
         keyboardType="numeric"
@@ -68,7 +74,6 @@ function TipsScreen() {
       <Text style={styles.label}>Type your question:</Text>
       <TextInput
         style={styles.textArea}
-        placeholder="Can you assist me in creating a meal plan for my child?"
         value={query}
         onChangeText={setQuery}
         multiline
@@ -79,11 +84,31 @@ function TipsScreen() {
         <Text style={styles.submitButtonText}>Submit</Text>
       </TouchableOpacity>
 
+      {/* Response Message Area */}
       {responseMessage && (
-        <ScrollView style={styles.responseContainer}>
-          <MarkdownDisplay content={responseMessage} />
-        </ScrollView>
+        <TouchableOpacity onPress={toggleModal} style={styles.modalOverlay}>
+          <Text style={styles.modalText}>Click here to read the response</Text>
+        </TouchableOpacity>
       )}
+
+      {/* Modal for displaying the response message */}
+      <Modal
+        visible={isModalVisible}
+        animationType="slide"
+        transparent={true}
+        onRequestClose={toggleModal}
+      >
+        <View style={styles.modalBackground}>
+          <View style={styles.modalContent}>
+            <TouchableOpacity onPress={toggleModal} style={styles.closeButton}>
+              <Icon name="close" size={28} color="#FFFFFF" />
+            </TouchableOpacity>
+            <ScrollView style={styles.responseContainer}>
+              <MarkdownDisplay content={responseMessage} />
+            </ScrollView>
+          </View>
+        </View>
+      </Modal>
     </View>
   );
 }
@@ -100,11 +125,13 @@ const styles = StyleSheet.create({
     fontSize: 24,
     fontWeight: 'bold',
     marginBottom: 10,
+    color: '#2C2A29',
   },
   text: {
     fontSize: 16,
     textAlign: 'center',
     marginBottom: 20,
+    color: '#4A4A4A',
   },
   label: {
     fontSize: 16,
@@ -147,20 +174,47 @@ const styles = StyleSheet.create({
     color: 'red',
     marginTop: 5,
   },
-  responseContainer: {
+  modalOverlay: {
     marginTop: 20,
-    padding: 15,
+    padding: 10,
+    backgroundColor: '#FFFFFF',
+    borderRadius: 5,
+    width: '90%',
+    alignItems: 'center',
+    paddingVertical: 20,
+    borderColor: '#000',
+    borderWidth: 1,
+  },
+  modalText: {
+    fontSize: 16,
+    color: '#000',
+    textAlign: 'center',
+  },
+  modalBackground: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(0, 0, 0, 0.7)', // semi-transparent background
+  },
+  modalContent: {
+    width: '90%',
+    maxHeight: screenHeight * 0.9, // Set a maximum height of 90% of screen height
     backgroundColor: '#FFFFFF',
     borderRadius: 10,
-    width: '100%',
-    maxHeight: 300, // Set a max height for the response container
-    borderColor: '#FF6F00',
-    borderWidth: 2,
+    padding: 20,
     shadowColor: '#000',
     shadowOffset: { width: 0, height: 2 },
     shadowOpacity: 0.3,
     shadowRadius: 5,
     elevation: 5, // Shadow for Android
+  },
+  closeButton: {
+    alignSelf: 'flex-end',
+    padding: 0,
+    backgroundColor: '#E7B549',
+  },
+  responseContainer: {
+    maxHeight: 500, // Set a max height for the response container
   },
 });
 
