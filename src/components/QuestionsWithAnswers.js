0f4c778780d490a7d0bb0e03aa0b1dd83a7c5367
harkamal <honey.rawal89@gmail.com>
2024-10-13 17:49:48 +0200

    QuestionWithAnswers history

--- src/components/QuestionsWithAnswers.js
+++ src/components/QuestionsWithAnswers.js
@@ -1,38 +1,54 @@
-import React, { useState } from 'react';
+import React from 'react';
 import {
   View,
   Text,
   TouchableOpacity, StyleSheet,
 } from 'react-native';
-import {guestUserQuestionScreenStyles} from '../styles/styles';
-import {useNavigation} from '@react-navigation/native';
+import { guestUserQuestionScreenStyles } from '../styles/styles';
+import { useNavigation } from '@react-navigation/native';
 
 const QuestionsWithAnswers = ({ questionsWithAnswers }) => {
-  const handleQuestionPress = (questionWithAnswer) => {
-    navigation.navigate('AnswerScreen', { responseMessage: questionWithAnswer.answer, question: questionWithAnswer.question });
-  };
   const navigation = useNavigation();
+
+  const handleQuestionPress = (questionWithAnswer) => {
+    navigation.navigate('AnswerScreen', {
+      responseMessage: questionWithAnswer.answer,
+      question: questionWithAnswer.question,
+      childAge: questionWithAnswer.childAgeInMonths,
+    });
+  };
 
   return (
     <View style={styles.questionHistoryContainer}>
-    <View style={guestUserQuestionScreenStyles.trendingQuestionsContainer}>
-      <TouchableOpacity style={guestUserQuestionScreenStyles.questionHeaderContainer}>
-        <Text style={guestUserQuestionScreenStyles.trendingQuestionsHeader}>
-          Your Questions
-        </Text>
-      </TouchableOpacity>
-      {questionsWithAnswers.map((item, index) => (
-          <TouchableOpacity key={index} onPress={() => handleQuestionPress(item)} style={guestUserQuestionScreenStyles.questionContainer}>
-            <View style={guestUserQuestionScreenStyles.questionRow}>
-              <Text style={guestUserQuestionScreenStyles.trendingQuestion}>{item.question}</Text>
-              <View style={guestUserQuestionScreenStyles.arrowIconContainer}>
-                <Text style={guestUserQuestionScreenStyles.arrowColor}> > </Text>
+      <View style={guestUserQuestionScreenStyles.trendingQuestionsContainer}>
+        <TouchableOpacity style={guestUserQuestionScreenStyles.questionHeaderContainer}>
+          <Text style={guestUserQuestionScreenStyles.trendingQuestionsHeader}>
+            Your Questions
+          </Text>
+        </TouchableOpacity>
+        {/* Check if questionsWithAnswers is empty or undefined */}
+        {questionsWithAnswers && questionsWithAnswers.length > 0 ? (
+          questionsWithAnswers.map((item, index) => (
+            <TouchableOpacity
+              key={index}
+              onPress={() => handleQuestionPress(item)}
+              style={guestUserQuestionScreenStyles.questionContainer}
+            >
+              <View style={guestUserQuestionScreenStyles.questionRow}>
+                <Text style={guestUserQuestionScreenStyles.trendingQuestion}>{item.question}(Child Age: {item.childAgeInMonths} Months)</Text>
+                <View style={guestUserQuestionScreenStyles.arrowIconContainer}>
+                  <Text style={guestUserQuestionScreenStyles.arrowColor}> > </Text>
+                </View>
               </View>
-            </View>
-          </TouchableOpacity>
-        ))
-      }
-    </View>
+            </TouchableOpacity>
+          ))
+        ) : (
+          // Fallback message when no questions exist
+          <Text style={styles.noQuestionsText}>
+            It looks like you haven’t asked any questions yet. Start exploring and let us help with your parenting journey!
+          </Text>
+        )}
+      </View>
     </View>
   );
 };
@@ -44,7 +60,11 @@ const styles = StyleSheet.create({
     backgroundColor: '#eff9ff',
     flex: 1,
     justifyContent: 'flex-start',
-    //alignItems: 'center',
     paddingTop: 40,
+  },
+  noQuestionsText: {
+    padding: 10,
+    fontSize: 16,
+    fontFamily: 'Montserrat-Medium',
   },
 });
